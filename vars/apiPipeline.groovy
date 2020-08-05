4b60a8dc2babb101e3ed2670d7d79e5a0ed9ac09
themanfromearth <ali.bakan@cloudnesil.com>
2020-08-05 21:10:29 +0300

    v5

--- vars/apiPipeline.groovy
+++ vars/apiPipeline.groovy
@@ -1,14 +1,19 @@
 #!/usr/bin/env groovy
 
+def callback() {
+    echo "hello you"
+}
+
 def call() {
     pipeline {
+        agent any
+
         stages {
             stage('Test') {
                 steps {
                     script {
-
                         echo "Testing..."
-                        createJiraTicket summary: "test me"
+                        createJiraTicket(summary: "test me", callback)
                         echo "Done"
                     }
                 }
