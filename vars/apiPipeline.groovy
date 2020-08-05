e6d5ab755fe7723e51d3977c3c4c7421d07278ef
themanfromearth <ali.bakan@cloudnesil.com>
2020-08-05 21:34:17 +0300

    v10

--- vars/apiPipeline.groovy
+++ vars/apiPipeline.groovy
@@ -10,12 +10,9 @@ def call(Map pipelineParams) {
                     script {
                         echo pipelineParams.param
                         echo pipelineParams.param2
-                        createJiraTicket(summary: "test me")
-                        createJiraTicket(summary: "Summary", description: "Description",
-                                 team: "inventory-management", application: "stock-hold") {
+                        createJiraTicket(summary: "test me") {
                             echo "i am insid of create jira issu"
                         }
-                        echo "Done"
                     }
                 }
             }
