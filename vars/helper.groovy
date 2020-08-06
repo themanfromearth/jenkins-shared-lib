39b7f62e81c5932dbc323c942f170c4184ac3c0d
themanfromearth <ali.bakan@cloudnesil.com>
2020-08-06 10:19:54 +0300

    v24

--- vars/helper.groovy
+++ vars/helper.groovy
@@ -2,8 +2,7 @@
 
 def call(cluster = "", numInstances = 1, envVars = []) {
 
-    echo cluster
-    echo numInstances
-    echo envVars.NEW_RELIC_ENVIRONMENT
+
+    print("${cluster} - ${numInstances} - ${envVars.NEW_RELIC_ENVIRONMENT}")
 
 }
